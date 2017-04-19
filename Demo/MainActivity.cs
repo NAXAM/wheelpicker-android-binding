9abcf4ce13b1d2e6b019905a6ccf949f5648964b
Morejump <luuvanthaoepu@gmail.com>
2017-04-19 11:38:45 +0700

    Add project files.

---
+++ Demo/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace BindingTest
+{
+    [Activity(Label = "BindingTest", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            initialMethod();
+        }
+
+        // initializing a necessary stuff
+        private void initialMethod()
+        {
+
+        }
+    }
+}
+
